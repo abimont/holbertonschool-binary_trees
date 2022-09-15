6a4613462f87d8e6c5470ad4235238fcad971430
abimont <abimontes00@gmail.com>
2022-09-14 20:22:04 -0700

    case fixed

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -12,19 +12,20 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
 	binary_tree_t *node;
 
-	if (!value)
-		return (NULL);
-
 	node = malloc(sizeof(binary_tree_t));
 	if (!node)
 		return (NULL);
 
 	node->n = value;
+	
 	if (parent == NULL)
 		parent = node;
 
 	if (parent != NULL)
 		node->parent = parent;
+
+	node->left = NULL;
+	node->right = NULL;
 
 	return (node);
 }
